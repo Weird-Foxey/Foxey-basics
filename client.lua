810e47fd52e10905415436456c97556bc941eb11
Weird_Foxey <46900190+Weird-Foxey@users.noreply.github.com>
2021-12-01 18:22:11 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,78 @@
+Citizen.CreateThread(function()
+    StartAudioScene("CHARACTER_CHANGE_IN_SKY_SCENE")
+    SetAudioFlag("PoliceScannerDisabled", true)
+	Citizen.Wait(500)
+    while true do
+	    local playerPed = PlayerPedId()
+		local pos = GetEntityCoords(playerPed) 
+		SetGarbageTrucks(0)
+		SetCreateRandomCops(0)
+		SetCreateRandomCopsNotOnScenarios(0)
+		SetCreateRandomCopsOnScenarios(0)
+		DistantCopCarSirens(0)
+		CancelCurrentPoliceReport()
+		RemoveVehiclesFromGeneratorsInArea(335.2616 - 300.0, -1432.455 - 300.0, 46.51 - 300.0, 335.2616 + 300.0, -1432.455 + 300.0, 46.51 + 300.0) -- central los santos medical center
+		RemoveVehiclesFromGeneratorsInArea(441.8465 - 500.0, -987.99 - 500.0, 30.68 -500.0, 441.8465 + 500.0, -987.99 + 500.0, 30.68 + 500.0) -- police station mission row
+		RemoveVehiclesFromGeneratorsInArea(316.79 - 300.0, -592.36 - 300.0, 43.28 - 300.0, 316.79 + 300.0, -592.36 + 300.0, 43.28 + 300.0) -- pillbox
+		RemoveVehiclesFromGeneratorsInArea(-2150.44 - 500.0, 3075.99 - 500.0, 32.8 - 500.0, -2150.44 + 500.0, -3075.99 + 500.0, 32.8 + 500.0) -- military
+		RemoveVehiclesFromGeneratorsInArea(-1108.35 - 300.0, 4920.64 - 300.0, 217.2 - 300.0, -1108.35 + 300.0, 4920.64 + 300.0, 217.2 + 300.0) -- nudist
+		RemoveVehiclesFromGeneratorsInArea(-458.24 - 300.0, 6019.81 - 300.0, 31.34 - 300.0, -458.24 + 300.0, 6019.81 + 300.0, 31.34 + 300.0) -- police station paleto
+		RemoveVehiclesFromGeneratorsInArea(1854.82 - 300.0, 3679.4 - 300.0, 33.82 - 300.0, 1854.82 + 300.0, 3679.4 + 300.0, 33.82 + 300.0) -- police station sandy
+		RemoveVehiclesFromGeneratorsInArea(-724.46 - 300.0, -1444.03 - 300.0, 5.0 - 300.0, -724.46 + 300.0, -1444.03 + 300.0, 5.0 + 300.0) -- REMOVE CHOPPERS WOW
+    	Citizen.Wait(1000)
+	end
+end)
+
+carblacklist = {
+	"police",
+	"police2",
+	"police3",
+	"police4",
+	"policeb",
+	"policet",
+	"policeold1",
+	"policeold2",
+	"pranger",
+	"fbi",
+	"fbi2",
+	"riot",
+	"sheriff",
+	"sheriff2",
+}
+
+Citizen.CreateThread(function()
+	while true do
+		Wait(1000)
+
+		playerPed = GetPlayerPed(-1)
+		if playerPed then
+			checkCar(GetVehiclePedIsIn(playerPed, false))
+
+			x, y, z = table.unpack(GetEntityCoords(playerPed, true))
+			for _, blacklistedCar in pairs(carblacklist) do
+				checkCar(GetClosestVehicle(x, y, z, 100.0, GetHashKey(blacklistedCar), 70))
+			end
+		end
+	end
+end)
+
+function checkCar(car)
+	if car then
+		carModel = GetEntityModel(car)
+		carName = GetDisplayNameFromVehicleModel(carModel)
+
+		if isCarBlacklisted(carModel) then
+			DeleteEntity(car)
+		end
+	end
+end
+
+function isCarBlacklisted(model)
+	for _, blacklistedCar in pairs(carblacklist) do
+		if model == GetHashKey(blacklistedCar) then
+			return true
+		end
+	end
+
+	return false
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Foxey#0505'
+description 'Foxey Anticheat'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
